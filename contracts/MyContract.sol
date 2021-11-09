f32bef92badf2ce71f1300122646594f8edcf9ca
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2021-11-09 16:04:42 -0500

    updated with deploy test

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,12 +3,11 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract MyContract {
 //all avaialble add spaces
-
-  mapping (uint => Message) adSpaces;
+  mapping (uint => bytes32) adSpaces;
   
 //Pick an adSpace # from the mapping and input a message to be displayed on the website
-  function buyAd(uint number, string message) public returns(bool) {
-    number.message = message;
+  function buyAd(uint number, bytes32 message) public returns(bool) {
+    adSpaces[number] = message;
     return true;
   }
 
@@ -23,8 +22,8 @@ contract MyContract {
   }
 
 //Return the message that is located at a certain mapping location
-  function get(uint number) public view returns (string) {
-    return message;
+  function get(uint number) public returns (bytes32) {
+    return adSpaces[number];
   }
 
 
---
+++ test/mycontract.js
@@ -0,0 +1,24 @@
+const MyContract = artifacts.require("./MyContract.sol");
+
+contract("MyContract", accounts => {
+    it("contract should be deployed", async () => {
+        await MyContract.deployed();
+        return assert.isTrue(true);
+    })
+});
+
+/*
+contract("SipmleStorage", accounts => {
+  it("...should store the value 89.", async () => {
+    const simpleStorageInstance = await SimpleStorage.deployed();
+
+    // Set value of 89
+    await simpleStorageInstance.set(89, { from: accounts[0] });
+
+    // Get stored value
+    const storedData = await simpleStorageInstance.get.call();
+
+    assert.equal(storedData, 89, "The value 89 was not stored.");
+  });
+});
+*/
