69f53964ddc6ce5e157ffc9bdb6e77ecb4919785
garyghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2021-11-15 11:08:32 -0500

    moved from truffle react box to truffle init

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,6 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const MyContract = artifacts.require("./MyContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+
   deployer.deploy(MyContract);
 };
